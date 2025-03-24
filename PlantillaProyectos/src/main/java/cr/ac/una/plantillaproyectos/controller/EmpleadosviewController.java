794661c866dd1bb4fc5ff45c48b775b9443d69f4
C-daniel-arias <148651307+C-daniel-arias@users.noreply.github.com>
2025-03-24 01:02:19 -0600

    cambios en view
    add view empeladosView y controller respectivo

--- PlantillaProyectos/src/main/java/cr/ac/una/plantillaproyectos/controller/EmpleadosviewController.java
+++ PlantillaProyectos/src/main/java/cr/ac/una/plantillaproyectos/controller/EmpleadosviewController.java
@@ -4,9 +4,20 @@
  */
 package cr.ac.una.plantillaproyectos.controller;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXCheckbox;
+import io.github.palexdev.materialfx.controls.MFXDatePicker;
+import io.github.palexdev.materialfx.controls.MFXPasswordField;
+import io.github.palexdev.materialfx.controls.MFXRadioButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -15,6 +26,47 @@ import javafx.fxml.Initializable;
  */
 public class EmpleadosviewController implements Initializable {
 
+    @FXML
+    private ToggleGroup Genero;
+    @FXML
+    private AnchorPane fondoPrin;
+    @FXML
+    private AnchorPane fondoSec;
+    @FXML
+    private Label lbControlEmpleados;
+    @FXML
+    private MFXTextField txfId;
+    @FXML
+    private MFXTextField txfNombre;
+    @FXML
+    private MFXTextField txfPrimerApellido;
+    @FXML
+    private MFXTextField txfSegundoApellido;
+    @FXML
+    private MFXTextField txfCedula;
+    @FXML
+    private MFXRadioButton rbutMasculino;
+    @FXML
+    private MFXRadioButton rbutFemenino;
+    @FXML
+    private MFXCheckbox cboxAdministrador;
+    @FXML
+    private MFXCheckbox cboxActivo;
+    @FXML
+    private MFXDatePicker dpFechaIngreso;
+    @FXML
+    private MFXTextField txfCorreo;
+    @FXML
+    private MFXPasswordField pasfClave;
+    @FXML
+    private MFXButton btnNuevo;
+    @FXML
+    private MFXButton btnBuscar;
+    @FXML
+    private MFXButton btnEleminar;
+    @FXML
+    private MFXButton btnGuardar;
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +74,21 @@ public class EmpleadosviewController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void onActionNuevo(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBuscar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionEliminar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionGuardar(ActionEvent event) {
+    }
     
 }
